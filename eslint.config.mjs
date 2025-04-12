2d9d886ef44b2fc9d62bb27687627e4f45135deb
Jaka Krajnc <444.ous@gmail.com>
2025-04-12 10:01:16 +0200

    🪇 update configs with new goodies from ixartz

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import antfu from "@antfu/eslint-config";
+import nextPlugin from "@next/eslint-plugin-next";
+import jestDom from "eslint-plugin-jest-dom";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import playwright from "eslint-plugin-playwright";
+import testingLibrary from "eslint-plugin-testing-library";
+import path from 'node:path'
+
+export default antfu(
+  {
+    react: true,
+    typescript: true,
+    lessOpinionated: true,
+    isInEditor: false,
+    stylistic: {
+      semi: true,
+    },
+    formatters: {
+      css: true,
+    },
+    ignores: ["migrations/**/*", "next-env.d.ts"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@libs": path.resolve(__dirname, "libs"),
+      "@public": path.resolve(__dirname, "public"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@ui": path.resolve(__dirname, "src/components/ui"),
+    },
+  },
+  jsxA11y.flatConfigs.recommended,
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+  {
+    files: ["**/*.test.ts?(x)"],
+    ...testingLibrary.configs["flat/react"],
+    ...jestDom.configs["flat/recommended"],
+  },
+  {
+    files: ["**/*.spec.ts", "**/*.e2e.ts"],
+    ...playwright.configs["flat/recommended"],
+  },
+  {
+    rules: {
+      "antfu/no-top-level-await": "off",
+      "style/brace-style": ["error", "1tbs"],
+      "ts/consistent-type-definitions": ["error", "type"],
+      "react/prefer-destructuring-assignment": "off",
+      "node/prefer-global/process": "off",
+      "test/padding-around-all": "error",
+      "test/prefer-lowercase-title": "off",
+    },
+  },
+);
